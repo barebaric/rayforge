68474d867c6a4182e6bf723f62d826422e985cfb
Samuel Abels <knipknap@gmail.com>
2026-08-15 16:12:59 +0200

    fix: require Gdk/Graphene versions at bootstrap and move sketcher UI test out of backend suite

--- rayforge/builtin_addons/rayforge-addon-sketcher/tests/conftest.py
+++ rayforge/builtin_addons/rayforge-addon-sketcher/tests/conftest.py
@@ -15,6 +15,8 @@ try:
     import gi
 
     gi.require_version("Gtk", "4.0")
+    gi.require_version("Gdk", "4.0")
+    gi.require_version("Graphene", "1.0")
     gi.require_version("Adw", "1")
     import gi.repository.Adw
 except (ValueError, ImportError):
