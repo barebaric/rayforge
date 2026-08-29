9ec93f2b31ed0dd14641d8dc02fc6a080b50ce0d
Samuel Abels <knipknap@gmail.com>
2026-08-29 16:07:21 +0200

    test(machine): use resolvable host in Ruida travel speed test

--- tests/machine/models/test_machine.py
+++ tests/machine/models/test_machine.py
@@ -1237,7 +1237,7 @@ class TestMachine:
         """A Ruida machine has no dialect but can emit travel speed."""
         await wait_for_tasks_to_finish(task_mgr)
         machine.set_dialect_uid(None)
-        machine.set_driver(RuidaDriver, {"host": "test"})
+        machine.set_driver(RuidaDriver, {"host": "localhost"})
         await wait_for_tasks_to_finish(task_mgr)
 
         assert machine.driver_name == "RuidaDriver"
